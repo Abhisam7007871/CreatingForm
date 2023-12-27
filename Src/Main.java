787f5295b12e039e6146819d796bd082290b7bd0
Abhishek <abhisam7007871@gmail.com>
2023-12-27 23:58:21 +0530

    [Started] Creating Form Project using Java Swing

--- Src/Main.java
+++ Src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("abhsihek Testing");
+
         MyWindow w = new MyWindow();
     }
 }
--- Src/MyWindow.java
+++ Src/MyWindow.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class MyWindow extends JFrame {
     //constructor
     public MyWindow(){
-
+        super();
         System.out.println("This is Constructor");
     }
 
